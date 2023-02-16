7eb3c363929279b06529c74f39f91827ff85d61d
Angelique15 <mar.angel.go15@gmail.com>
2023-02-15 20:22:43 -0500

    biblioteca SimpleLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 const gallery = document.querySelector(".gallery");
-// Crear un nuevo array de elementos <a> que contienen imágenes y títulos
+// Nuevo array de elementos <a> que contienen imágenes y títulos
 const galleryElements = galleryItems.map(item => `
   <a href="${item.original}" class="gallery__item">
     <img src="${item.preview}" alt="${item.description}" class="gallery__image">
@@ -11,25 +10,17 @@ const galleryElements = galleryItems.map(item => `
 
 gallery.innerHTML = galleryElements;
 
-const lightbox = basicLightbox.create(`
-  <div class="modal">
-    <img src="" alt="" class="modal__image">
-  </div>
-`, {
-    onShow: (instance) => {
-        const modalImage = instance.element().querySelector('.modal__image');
-        const galleryItem = gallery.querySelector('.gallery__item--current');
-        modalImage.src = galleryItem.href;
-    }
-});
-
-gallery.addEventListener('click', (event) => {
+// Evento de click a cada imagen
+const images = gallery.querySelectorAll(".gallery__image");
+images.forEach(image => {
+  image.addEventListener("click", event => {
     event.preventDefault();
+    const source = image.dataset.source;
+    const title = image.alt;
+    const lightbox = basicLightbox.create(`<img src="${source}" alt="${title}">`);
     lightbox.show();
-    event.currentTarget.querySelector('.gallery__item').classList.add('gallery__item--current');
+  });
 });
-
-
 console.log(galleryItems);
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+// Nuevo array de elementos <a> que contienen imágenes y subtítulos
+const galleryElements = galleryItems.map(item => `
+  <a href="${item.original}" class="gallery__item" data-subtitle="${item.description}">
+    <img src="${item.preview}" alt="${item.description}" class="gallery__image" data-source="${item.original}">
+  </a>
+`).join('');
+
+gallery.innerHTML = galleryElements;
+
+// Evento de click a cada imagen
+const images = gallery.querySelectorAll(".gallery__image");
+images.forEach(image => {
+    image.addEventListener("click", event => {
+        event.preventDefault();
+        const source = image.dataset.source;
+        const title = image.alt;
+        const lightbox = basicLightbox.create(`<img src="${source}" alt="${title}">`);
+        lightbox.show();
+    });
+});
+
+// Inicio de la biblioteca SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true, // Mostrar subtítulos
+    captionDelay: 250 // Retraso de 250 milisegundos para mostrar el subtítulo
+});
+
+console.log(lightbox);
 console.log(galleryItems);
